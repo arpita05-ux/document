587ae4fa190243aabb41618dc38c4a54d3299779
Arpita <arpitakumari10653@gmail.com>
2025-05-19 20:56:08 +0530

    first commit

---
+++ ar.java
@@ -0,0 +1,11 @@
+public class AboutMe {
+     public static void main(String[] args) {
+        System.out.println("My name is Arpita.");
+        system.out.println("My age is 17.");
+        System.out.println("I passed 12th.");
+        System.out.println("I recently started coding.");
+        System.out.println("progress is loading.");
+
+
+        system.out.print.ln("learning java initial stage");
+        
